e067a451185a24f21765b3b0150fb45269580b7a
HoHoangLinh <linh0905171522@gmail.com>
2022-10-26 21:00:15 +0700

    BaiTap

--- module2/src/ss5_Access_modifer/BaiTap/Student/Main.java
+++ module2/src/ss5_Access_modifer/BaiTap/Student/Main.java
@@ -1,15 +1,11 @@
 package ss5_Access_modifer.BaiTap.Student;
 
 public class Main {
-    void method(int x, int y) {
-        x = 5;
-        y = 5;
-    }
-
     public static void main(String[] args) {
-        Main o = new Main();
-        int x = 1, y = 1;
-        o.method(x, y);
-        System.out.printf("x=%d, y=%d", x, y);
+        Student student = new Student();
+        System.out.println(student.getClasses() + " " + student.getName());
+        student.setName("Hoa");
+        student.setClasses("C03");
+        System.out.println(student.getClasses() + " " + student.getName());
     }
 }
